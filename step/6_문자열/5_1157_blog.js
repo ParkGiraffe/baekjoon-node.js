795d1cb8238685b433dc89114e316ddc547ae1d6
ParkGiraffe <op5321@soongsil.ac.kr>
2023-02-01 15:58:24 +0900

    slove a step 6 question

---
+++ step/6_문자열/5_1157_blog.js
@@ -0,0 +1,70 @@
+/*
+
+문제
+알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+
+입력
+첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+
+출력
+첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+
+예제 입력 1 
+Mississipi
+예제 출력 1 
+?
+
+예제 입력 2 
+zZa
+예제 출력 2 
+Z
+
+예제 입력 3 
+z
+예제 출력 3 
+Z
+
+예제 입력 4 
+baaa
+예제 출력 4 
+A
+
+*/
+
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("");
+const strTable = [];
+const countTable = [];
+
+input.forEach((element) => {
+  const e = element.toUpperCase();
+  const idx = strTable.findIndex((c) => c === e);
+  // console.log(idx)
+  if (idx !== -1) {
+    ++countTable[idx];
+  } else {
+    strTable.push(e);
+    countTable.push(1);
+  }
+});
+
+// console.log(strTable, countTable)
+
+const findTop = (countTable) => {
+  const idxTable = countTable.reduce(
+    (acc, cur, i) => {
+      // console.log(acc)
+      if (acc[0] < cur) return [cur, 1];
+      if (acc[0] === cur) return [cur, acc[1] + 1];
+      if (acc[0] > cur) return acc;
+    },
+    [0, 0]
+  );
+
+  if (idxTable[1] > 1) return -1;
+  return countTable.findIndex((e) => e === idxTable[0]);
+};
+
+const idx = findTop(countTable);
+result = idx === -1 ? "?" : strTable[idx];
+console.log(result);
