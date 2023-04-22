11bd2266ce4e630c89b4daaa7f04a09947760a7f
ParkGiraffe <op5321@soongsil.ac.kr>
2023-04-22 23:07:31 +0900

    solve a step 17 question

---
+++ step/17_조합론/4_11050.js
@@ -0,0 +1,45 @@
+/*
+이항 계수 1
+https://www.acmicpc.net/problem/11050
+
+문제
+자연수 
+\(N\)과 정수 
+\(K\)가 주어졌을 때 이항 계수 
+\(\binom{N}{K}\)를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 
+\(N\)과 
+\(K\)가 주어진다. (1 ≤ 
+\(N\) ≤ 10, 0 ≤ 
+\(K\) ≤ 
+\(N\))
+
+출력
+ 
+\(\binom{N}{K}\)를 출력한다.
+
+예제 입력 1 
+5 2
+예제 출력 1 
+10
+*/
+
+const fs = require('fs');
+const [N, K] = fs.readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
+
+let n = 1;
+let r = 1;
+
+for (let i = N-K+1; i <= N; i++) {
+  n *= i;
+}
+
+for (let i = 1; i <= K; i++) {
+  r *= i
+}
+
+
+console.log(n / r);
+
