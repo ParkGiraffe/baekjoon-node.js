6847e1aca867cb2c6b95ca4ca3b86b34cb312aa0
ParkGiraffe <op5321@soongsil.ac.kr>
2023-05-24 14:09:19 +0900

    Fix 21-7

--- step/21_재귀/7_11729.js
+++ step/21_재귀/7_11729.js
@@ -35,15 +35,18 @@ https://www.acmicpc.net/problem/11729
 */
 const fs = require("fs");
 const input = +fs.readFileSync("/dev/stdin").toString().trim();
+let output = "";
 
 const hanoi = (n, start, end) => {
-  if (n === 1) console.log(start, end);
+  if (n === 1) output += start + " " + end + "\n";
   else {
     hanoi(n - 1, start, 6 - start - end);
-    console.log(start, end);
+    output += start + " " + end + "\n";
     hanoi(n - 1, 6 - start - end, end);
   }
 };
 
-console.log(2 ** input - 1);
+output += `${2 ** input - 1}\n`;
 hanoi(input, 1, 3);
+
+console.log(output);
