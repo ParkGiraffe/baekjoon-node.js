b5738885cee3d32f31dfadecaaaef01e17a2157c
ParkGiraffe <op5321@soongsil.ac.kr>
2023-11-09 14:29:47 +0900

    solve a step 36 question

---
+++ step/36_유니온_파인드/5_9372_blog.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const [first, ...second] = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const tCount = +first;
+let cursor = 0;
+let output = [];
+
+for (let t = 0; t < tCount; t++) {
+  const [n, m] = second[cursor++].split(" ").map(Number);
+  //   console.log(n, m);
+  output.push(n - 1);
+  cursor += m;
+}
+
+console.log(output.join("\n"));
