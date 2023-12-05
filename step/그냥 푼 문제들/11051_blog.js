89069f19d6d5503f2055aa0d60627d2e136a7769
ParkGiraffe <op5321@soongsil.ac.kr>
2023-12-05 10:16:50 +0900

    solve any question

--- step/그냥 푼 문제들/11051_blog.js
+++ step/그냥 푼 문제들/11051_blog.js
@@ -1,31 +1,32 @@
 /*
-이항계수
-https://www.acmicpc.net/problem/11050
-
+이항 계수 2
+https://www.acmicpc.net/problem/11051
 
 문제
 자연수 
 \(N\)과 정수 
 \(K\)가 주어졌을 때 이항 계수 
-\(\binom{N}{K}\)를 구하는 프로그램을 작성하시오.
+\(\binom{N}{K}\)를 10,007로 나눈 나머지를 구하는 프로그램을 작성하시오.
 
 입력
 첫째 줄에 
 \(N\)과 
 \(K\)가 주어진다. (1 ≤ 
-\(N\) ≤ 10, 0 ≤ 
+\(N\) ≤ 1,000, 0 ≤ 
 \(K\) ≤ 
 \(N\))
 
 출력
  
-\(\binom{N}{K}\)를 출력한다.
+\(\binom{N}{K}\)를 10,007로 나눈 나머지를 출력한다.
 
 예제 입력 1 
 5 2
 예제 출력 1 
 10
+
 */
+
 const fs = require("fs");
 const [n, k] = fs
   .readFileSync("/dev/stdin")
@@ -41,7 +42,8 @@ const setFactorial = (n, k) => {
 
   if (k === 0 || n === k) return (dp[n][k] = 1);
 
-  return (dp[n][k] = setFactorial(n - 1, k - 1) + setFactorial(n - 1, k));
+  return (dp[n][k] =
+    (setFactorial(n - 1, k - 1) + setFactorial(n - 1, k)) % 10007);
 };
 
 console.log(setFactorial(n, k)); // top-down
