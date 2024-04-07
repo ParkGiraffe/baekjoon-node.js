9555566e850c4645eb392f08e696a0baf5b49f7c
ParkGiraffe <op5321@soongsil.ac.kr>
2024-04-08 00:06:25 +0900

    solve any question

---
+++ step/그냥 푼 문제들/2xxx/2438.js
@@ -0,0 +1,37 @@
+/*
+별 찍기 - 1
+https://www.acmicpc.net/problem/2438
+
+문제
+첫째 줄에는 별 1개, 둘째 줄에는 별 2개, N번째 줄에는 별 N개를 찍는 문제
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 N번째 줄까지 차례대로 별을 출력한다.
+
+예제 입력 1 
+5
+예제 출력 1 
+*
+**
+***
+****
+*****
+*/
+
+const fs = require("fs");
+const N = +fs.readFileSync("/dev/stdin").toString().trim();
+
+const output = [];
+for (let i = 1; i <= N; i++) {
+  const temp = [];
+  for (let j = 0; j < i; j++) {
+    temp.push("*");
+  }
+
+  output.push(temp.join(""));
+}
+
+console.log(output.join("\n"));
