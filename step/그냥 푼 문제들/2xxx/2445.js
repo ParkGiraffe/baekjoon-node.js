7abc5d372b8c2ecd472849eeb7f782e7d71f6853
ParkGiraffe <op5321@soongsil.ac.kr>
2024-04-18 16:15:07 +0900

    solve any question

---
+++ step/그냥 푼 문제들/2xxx/2445.js
@@ -0,0 +1,68 @@
+/*
+별 찍기 - 8
+https://www.acmicpc.net/problem/2445
+
+문제
+예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+
+입력
+첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+
+출력
+첫째 줄부터 2×N-1번째 줄까지 차례대로 별을 출력한다.
+
+예제 입력 1 
+5
+예제 출력 1 
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+*/
+const fs = require("fs");
+const N = +fs.readFileSync("/dev/stdin").toString().trim();
+
+const output = [];
+
+for (let i = 1; i <= N; i++) {
+  const temp = [];
+
+  for (let j = 0; j < i; j++) {
+    temp.push("*");
+  }
+
+  for (let j = 0; j < 2 * (N - i); j++) {
+    temp.push(" ");
+  }
+
+  for (let j = 0; j < i; j++) {
+    temp.push("*");
+  }
+
+  output.push(temp.join(""));
+}
+
+for (let i = N - 1; i >= 1; i--) {
+  const temp = [];
+
+  for (let j = 0; j < i; j++) {
+    temp.push("*");
+  }
+
+  for (let j = 0; j < 2 * (N - i); j++) {
+    temp.push(" ");
+  }
+
+  for (let j = 0; j < i; j++) {
+    temp.push("*");
+  }
+
+  output.push(temp.join(""));
+}
+
+console.log(output.join("\n"));
